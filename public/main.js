ddcd3d3404b4ac7945a10586767e3ea7c5996fd7
SONE, Mio <alice040qmarch>
2021-07-21 19:55:43 +0900

    change main

--- public/main.js
+++ public/main.js
@@ -1,32 +1,15 @@
 const socketio = io();
 const form = document.getElementById("form");
-const input = document.getElementById("msg");
+const input = document.getElementByID("msg");
 const chats = document.getElementById("chats");
 
-const nameForm = document.getElementById("nameForm");
-const userName = document.getElementById("userName");
-
-let username='';
-nameForm.addEventListener('submit', function(event){
-  if(userName.value!==''){
-    username = userName.value;
-    nameForm.style.display ="none";
-    form.style.display ="flex";
-
-    socketio.emit('signin');
-
-    const msg = {msg: username + ' さんが参加しました。', name: 'システム'};
-    socketio.emit('message', msg);
-  }
-
-  event.preventDefault();
+form.addEventListner('submit', function(event){
+socketio.emit('message', input.value);
+input.value='';
+event.preventDefault();
 })
-
-form.addEventListener('submit', function(event){
-  if(input.value!==''){
-    const msg =  {msg: input.value, name: username};
-    socketio.emit('message', msg);
-    input.value='';
-  }
-  event.preventDefault();
-})
+socketio.on('message',function(data){
+  const li = document.createElement("li");
+  $(li).append(msg)
+  chats.append(li);
+});
