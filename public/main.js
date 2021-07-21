5b63ae0e81f794d9374caaae8eb6007ef86bc76d
SONE, Mio <alice040qmarch>
2021-07-22 00:17:26 +0900

    change

--- public/main.js
+++ public/main.js
@@ -1,53 +1,54 @@
 const socketio = io();
 const form = document.getElementById("form");
-const input = document.getElementByID("msg");
+const input = document.getElementById("msg");
 const chats = document.getElementById("chats");
 
-const nameForm = document.getElementByID("nameForm");
-const nameForm = document.getElementByID("userName");
+const nameForm = document.getElementById("nameForm");
+const userName = document.getElementById("userName");
 
 let username='';
-nameForm.addEventListner('submit', function(event){
-  if(username.value!==''){
-    username= userName.value;
+nameForm.addEventListener('submit', function(event){
+  if(userName.value!==''){
+    username = userName.value;
     nameForm.style.display ="none";
     form.style.display ="flex";
 
     socketio.emit('signin');
 
-    const msg = {msg: username + ' さんが参加しました♪　', name: 'システム'};
+    const msg = {msg: username + ' さんが参加しました。', name: 'システム'};
     socketio.emit('message', msg);
   }
- event.preventDefault();
+
+  event.preventDefault();
 })
 
 
-form.addEventListner('submit', function(event){
+form.addEventListener('submit', function(event){
   if(input.value!==''){
-  const msg = {msg: input.value, name: username};  
-  socketio.emit('message', msg);
-input.value='';
-}
-event.preventDefault();
+    const msg = {msg: input.value, name: username};
+    socketio.emit('message', msg);
+    input.value='';
+  }
+  event.preventDefault();
 })
 
 socketio.on('message',function(msg){
   displayMessage(msg);
 });
 
-//参加時に過去のメッセージを受け取る
-socketio.on('signin',function(msg){
+// 参加時に過去のメッセージを受け取る
+socketio.on('signin',function(msgs){
   for(let i=0;i<msgs.length;i++){
-    const msg = msgs[1];
+    const msg = msgs[i];
     displayMessage(msg);
   }
 });
 
 function displayMessage(msg){
-  const dt = document.createElement("dt");
-  const dd = document.createElement("dd");
-  dt.append(msg.name);
-  chats.append(dt)
-  dd.append(msg.msg);
-  chats.append(dd);
-}
+    const dt = document.createElement("dt");
+    const dd = document.createElement("dd");
+    dt.append(msg.name);
+    chats.append(dt);
+    dd.append(msg.msg);
+    chats.append(dd);
+  }
