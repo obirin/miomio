1d44f019a57f7fa7d6ee31f708c979ebaa54886c
SONE, Mio <alice040qmarch>
2021-07-21 22:38:29 +0900

    add from video

--- public/main.js
+++ public/main.js
@@ -3,15 +3,35 @@ const form = document.getElementById("form");
 const input = document.getElementByID("msg");
 const chats = document.getElementById("chats");
 
+const nameForm = document.getElementByID("nameForm");
+const nameForm = document.getElementByID("userName");
+
+let username='';
+nameForm.addEventListner('submit', function(event){
+  if(username.value!==''){
+    username= userName.value;
+    nameForm.style.display ="none";
+    form.style.display ="flex";
+
+    socketio.emit('signin');
+
+    const msg = {msg: username + ' さんが参加しました♪　', name: 'システム'};
+    socketio.emit('message', msg);
+  }
+ event.preventDefault();
+})
+
 form.addEventListner('submit', function(event){
   if(input.value!==''){
-  socketio.emit('message', input.value);
+  const msg = {msg: input.value, name: username};  
+  socketio.emit('message', msg);
 input.value='';
 }
 event.preventDefault();
 })
+
 socketio.on('message',function(msg){
   const li = document.createElement("li");
-  li.append(msg);
+  li.append(msg.msg + msg.name + ')');
   chats.append(li);
 });
